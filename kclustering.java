675e80620674f5adaa3b30053fe46c4f2c4f6078
KOTESWARARAO6 <69566712+KOTESWARARAO6@users.noreply.github.com>
2021-09-21 15:28:41 +0530

    Create kclustering.java

---
+++ kclustering.java
@@ -0,0 +1,3 @@
+1 lines (1 sloc)  825 Bytes
+   
+ Arbitrarily choose k objects in D as the initial representative objects or seeds;RepeatAssign each remaining object to the cluster with the nearest representative object;Randomly se
